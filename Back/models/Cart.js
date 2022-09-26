61365c7d81e39c2821fd65882478a7b9fafe2c55
azizbannour1 <aziz.bannour@esprit.tn>
2022-09-26 23:38:59 +0100

    Final Commit

---
+++ Back/models/Cart.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    
+    date:{
+      type:Date,
+      default:Date.now
+  }
+
+  },
+);
+
+module.exports = mongoose.model("Cart", CartSchema);
---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTM-TYbemjZr64LP8mTPMN8dpndMq7G0E",
+  authDomain: "shop-82bdb.firebaseapp.com",
+  projectId: "shop-82bdb",
+  storageBucket: "shop-82bdb.appspot.com",
+  messagingSenderId: "736850160478",
+  appId: "1:736850160478:web:12e3686013908440e53dcf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
